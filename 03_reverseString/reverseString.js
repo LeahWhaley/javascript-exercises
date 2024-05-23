9e3b8d17051988decf9e4311c90a8a7876823ad3
Leah Whaley <leahwhaley26@gmail.com>
2024-05-22 22:54:03 -0400

    Complete exercise 3 to practice with JS and Jest

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let stringArray = originalString.split("");
+    stringArray.reverse();
+    let reversedString = stringArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
