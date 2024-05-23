a4e40cccf76434b880c43efc6a5ef21c8906f5a8
Leah Whaley <leahwhaley26@gmail.com>
2024-05-23 16:51:58 -0400

    Complete exercise four to practice with JS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    let newArray = [];
 
+    array.forEach(element => {
+        if (!values.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
