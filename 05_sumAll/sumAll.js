5b2b172f4df046ea77dfd4bf8085cb36efb361a5
Leah Whaley <leahwhaley26@gmail.com>
2024-05-23 16:59:57 -0400

    Complete exercise five to get practice with JS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstBound, secondBound) {
+    if (typeof(firstBound) !== "number" || typeof(secondBound) !== "number") {
+        return "ERROR";
+    } else if (firstBound < 0 || secondBound < 0) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    let start = firstBound;
+    let end = secondBound;
+    if (firstBound > secondBound) {
+        start = secondBound;
+        end = firstBound;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
