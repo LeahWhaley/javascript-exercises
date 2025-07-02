9cbedeee9f631777e1e3c77f7ae4d41d8decc5d6
Leah Whaley <leahwhaley26@gmail.com>
2025-07-02 15:16:59 -0400

    Complete exercise ten to practice with JS

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if ( index < 0 ) {
+        return "OOPS";
+    } else if ( index == 0 ) {
+        return 0;
+    } else if ( index == 1 || index == 2 ) {
+        return 1;
+    } else {
+        let prev1 = 1;
+        let prev2 = 1;
+        let current = 0;
+        for (let i = 3; i <= index; i++ ) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current
+        }
+        return current;
+    }
 
 };
 
