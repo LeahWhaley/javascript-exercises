ec850f3f96f4d51e806f0e78df6c18671f0d95aa
Leah Whaley <leahwhaley26@gmail.com>
2025-07-02 15:49:33 -0400

    Complete exercise twelve to practice JS

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => {
+        let ageA = 0;
+        let ageB = 0;
+        if ("yearOfDeath" in a === false) {
+            ageA = new Date().getFullYear() - a.yearOfBirth;
+        } else {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
 
+        if ("yearOfDeath" in b === false) {
+            ageB = new Date().getFullYear() - b.yearOfBirth;
+        } else {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+        return ageB - ageA;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
