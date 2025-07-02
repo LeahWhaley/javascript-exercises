b391c2d674649084a91efc0bd0def503852ba4f4
Leah Whaley <leahwhaley26@gmail.com>
2025-07-02 15:05:42 -0400

    Complete exercise nine to practice with JS

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let newString = "";
+    string = string.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+        if (characters.includes(string[i]) ) {
+            newString += string[i];
+        }
+    }
+    for (let j = 0; j < newString.length / 2; j++) {
+        if (newString[j] !== newString[newString.length - (j + 1)]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
